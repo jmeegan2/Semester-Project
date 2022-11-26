9cc6537e3488e84c55968ad0fdc31fcb5ebe0217
BuildTools <unconfigured@null.spigotmc.org>
2022-11-26 18:46:18 -0500

    added genre list implimentation

--- script.js
+++ script.js
@@ -141,9 +141,9 @@ const APPController = (function(UICtrl, APICtrl) {
         if (compare(prompts, replies, text)) { 
           // Search for exact match in `prompts`
           product = compare(prompts, replies, text);
-        } else if (text.match(/electronic/gi)) {
-          product = "You chose Genre: Electronic";
-          genresRecommend = "electronic";
+        } else if (text.match(/(electronic|acoustic|rock|rap|hiphop|classical|country|indie|romance|jazz|soul)/gi)) {
+          product = "You chose Genre: "+product;
+          genresRecommend = product;
         } else if (text.match(/(corona|covid|virus)/gi)) {
           // If no match, check if message contains `coronavirus`
           product = coronavirus[Math.floor(Math.random() * coronavirus.length)];
